f04bdaf2f6ec7e9532a646810c95a527ba00f99f
Getinet Amare <getinetamare@gmail.com>
2022-12-06 13:52:00 +0300

    Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - Check if a linked list has a cycle in it.
+ * @list: Value check.
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+*/
+int check_cycle(listint_t *list)
+{
+
+	listint_t *tortuga = list;
+	listint_t *conejo = list;
+
+	while (conejo && conejo->next)
+	{
+		tortuga = tortuga->next;
+		conejo = conejo->next->next;
+
+		if (tortuga == conejo)
+			return (1);
+	}
+
+	return (0);
+}
