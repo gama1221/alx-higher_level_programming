a82ec559230a27bf9a3044a3b25f89cc1275d1bf
Getinet <getinetamare@gmail.com>
2022-12-19 13:35:24 +0300

    Update 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+# 102-magic_calculation.py
+# Getinet Amare <getinetamare@gmail.com>
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
-            result += a ** b / i
+            else:
+                result += a ** b / i
         except:
-            result = a + b
+            result = b + a
             break
-    return result
+    return (result)
