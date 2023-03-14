588c56e1937061a928a24e066c412ece742a4224
getinet amare <getinetamare@gmail.com>
2023-03-13 21:33:43 -0700

    add a file

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-//Script that prints JavaScript is amazing text
-const myVar = 'JavaScript is amazing';
+// script that prints “Javascript is amazing”
+const myVar = 'Javascript is amazing';
 console.log(myVar);
