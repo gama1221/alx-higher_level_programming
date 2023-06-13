7a31c38addaa16b5dc432f6983f40053230e234a
Getinet <getinetamare@gmail.com>
2023-06-13 09:33:21 +0300

    Update 0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,1 +1,10 @@
+#!/usr/bin/node
 
+// Write a script that prints “JavaScript is amazing”:
+
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
+
+// You must create a constant variable called myVar with the value “JavaScript is amazing”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
