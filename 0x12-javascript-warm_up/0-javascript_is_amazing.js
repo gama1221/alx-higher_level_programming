339a71e612f695d040b0dc506e18afd2500be079
Getinet <getinetamare@gmail.com>
2023-06-13 09:20:06 +0300

    Update 0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-// Prints the string 'Javascript is amazing';
+// Script that prints "JavaScript is amazing".
 
-const myVar = 'Javascript is amazing';
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
