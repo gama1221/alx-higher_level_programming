07262bb65ff0a99f73461822c81834275c97b753
Getinet <getinetamare@gmail.com>
2023-06-13 09:58:04 +0300

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-// Write a script that prints 3 lines
+// Script that prints C is fun,Python is cool and JavaScript is amazing.
 
-const myVar = 'C is fun\nPython is cool\nJavascript is amazing';
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
 console.log(myVar);
